edf60e9fe2c0e882c3e3762149b521b551319371
Michael Liao <askxuefeng@gmail.com>
2012-09-26 15:34:31 +0800

    create pypi install script

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+import sys
+
+kw = dict(
+    name = 'sinaweibopy',
+    version = '1.0.5',
+    description = 'Sina Weibo OAuth2 API Python SDK',
+    long_description = open('README', 'r').read(),
+    author = 'Michael Liao',
+    author_email = 'askxuefeng@gmail.com',
+    url = 'https://github.com/michaelliao/sinaweibopy',
+    download_url = 'https://github.com/michaelliao/sinaweibopy',
+    py_modules = ['weibo'],
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ])
+
+if sys.version_info[1]==5:
+    kw['install_requires'] = ['simplejson']
+
+setup(**kw)
--- weibo.py
+++ weibo.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__version__ = '1.04'
+__version__ = '1.0.5'
 __author__ = 'Liao Xuefeng (askxuefeng@gmail.com)'
 
 '''
