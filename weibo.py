7bb92e0c8a67e7a8405e6adeedb838e6c55d4b1d
Michael Liao <askxuefeng@gmail.com>
2012-11-10 17:55:41 +0800

    add new-style api calling

--- weibo.py
+++ weibo.py
@@ -12,19 +12,8 @@ try:
     import json
 except ImportError:
     import simplejson as json
-import time
-import urllib
-import urllib2
-import logging
 
-def _obj_hook(pairs):
-    '''
-    convert json object to python object.
-    '''
-    o = JsonObject()
-    for k, v in pairs.iteritems():
-        o[str(k)] = v
-    return o
+import time, urllib, urllib2, logging, mimetypes
 
 class APIError(StandardError):
     '''
@@ -39,10 +28,19 @@ class APIError(StandardError):
     def __str__(self):
         return 'APIError: %s: %s, request: %s' % (self.error_code, self.error, self.request)
 
-class JsonObject(dict):
-    '''
-    general json object that can bind any fields but also act as a dict.
-    '''
+def _parse_json(s):
+    ' parse str to JsonDict '
+
+    def _obj_hook(pairs):
+        ' convert json object to python object '
+        o = JsonDict()
+        for k, v in pairs.iteritems():
+            o[str(k)] = v
+        return o
+    return json.loads(s, object_hook=_obj_hook)
+
+class JsonDict(dict):
+    ' general json object that can bind any fields but also act as a dict '
     def __getattr__(self, attr):
         return self[attr]
 
@@ -56,9 +54,7 @@ class JsonObject(dict):
         self.update(state)
 
 def _encode_params(**kw):
-    '''
-    Encode parameters.
-    '''
+    ' do url-encode parameters '
     args = []
     for k, v in kw.iteritems():
         qv = v.encode('utf-8') if isinstance(v, unicode) else str(v)
@@ -66,24 +62,18 @@ def _encode_params(**kw):
     return '&'.join(args)
 
 def _encode_multipart(**kw):
-    '''
-    Build a multipart/form-data body with generated random boundary.
-    '''
+    ' build a multipart/form-data body with generated random boundary '
     boundary = '----------%s' % hex(int(time.time() * 1000))
     data = []
     for k, v in kw.iteritems():
         data.append('--%s' % boundary)
         if hasattr(v, 'read'):
             # file-like object:
-            ext = ''
             filename = getattr(v, 'name', '')
-            n = filename.rfind('.')
-            if n != (-1):
-                ext = filename[n:].lower()
             content = v.read()
             data.append('Content-Disposition: form-data; name="%s"; filename="hidden"' % k)
             data.append('Content-Length: %d' % len(content))
-            data.append('Content-Type: %s\r\n' % _guess_content_type(ext))
+            data.append('Content-Type: %s\r\n' % _guess_content_type(filename))
             data.append(content)
         else:
             data.append('Content-Disposition: form-data; name="%s"\r\n' % k)
@@ -91,10 +81,12 @@ def _encode_multipart(**kw):
     data.append('--%s--\r\n' % boundary)
     return '\r\n'.join(data), boundary
 
-_CONTENT_TYPES = { '.png': 'image/png', '.gif': 'image/gif', '.jpg': 'image/jpeg', '.jpeg': 'image/jpeg', '.jpe': 'image/jpeg' }
-
-def _guess_content_type(ext):
-    return _CONTENT_TYPES.get(ext, 'application/octet-stream')
+def _guess_content_type(url):
+    n = url.rfind('.')
+    if n==(-1):
+        return 'application/octet-stream'
+    ext = url[n:]
+    mimetypes.types_map.get(ext, 'application/octet-stream')
 
 _HTTP_GET = 0
 _HTTP_POST = 1
@@ -137,12 +129,12 @@ def _http_call(the_url, method, authorization, **kw):
     try:
         resp = urllib2.urlopen(req)
         body = resp.read()
-        r = json.loads(body, object_hook=_obj_hook)
+        r = _parse_json(body)
         if hasattr(r, 'error_code'):
             raise APIError(r.error_code, r.get('error', ''), r.get('request', ''))
         return r
     except urllib2.HTTPError, e:
-        r = json.loads(e.read(), object_hook=_obj_hook)
+        r = _parse_json(e.read())
         if hasattr(r, 'error_code'):
             raise APIError(r.error_code, r.get('error', ''), r.get('request', ''))
         raise
@@ -213,7 +205,7 @@ class APIClient(object):
             rtime = int(remind_in) + current
             if rtime < expires:
                 expires = rtime
-        jo = JsonObject(access_token=r.access_token, expires_in=expires)
+        jo = JsonDict(access_token=r.access_token, expires_in=expires)
         uid = r.get('uid', None)
         if uid:
             jo.uid = uid
@@ -223,4 +215,110 @@ class APIClient(object):
         return not self.access_token or time.time() > self.expires
 
     def __getattr__(self, attr):
-        return getattr(self.get, attr)
+        if '__' in attr:
+            return getattr(self.get, attr)
+        return _Callable(self, attr)
+
+class WeiboClient(object):
+    '''
+    Weibo API client using synchronized invocation.
+    '''
+    def __init__(self, app_key, app_secret, redirect_uri=None, response_type='code', domain='api.weibo.com', version='2'):
+        self._client_id = app_key
+        self._client_secret = app_secret
+        self._redirect_uri = redirect_uri
+        self._response_type = response_type
+        self._auth_url = 'https://%s/oauth2/' % domain
+        self._api_url = 'https://%s/%s/' % (domain, version)
+        self._access_token = None
+        self._expires = 0.0
+
+    def set_access_token(self, access_token, expires_in):
+        self._access_token = str(access_token)
+        self._expires = float(expires_in)
+
+    def get_authorize_url(self, redirect_uri=None, display='default'):
+        '''
+        return the authroize url that should be redirect.
+        '''
+        redirect = redirect_uri if redirect_uri else self._redirect_uri
+        if not redirect:
+            raise APIError('21305', 'Parameter absent: redirect_uri', 'OAuth2 request')
+        return '%s%s?%s' % (self._auth_url, 'authorize', \
+                _encode_params(client_id = self._client_id, \
+                        response_type = 'code', \
+                        display = display, \
+                        redirect_uri = redirect))
+
+    def request_access_token(self, code, redirect_uri=None):
+        '''
+        return access token as object: {"access_token":"your-access-token","expires_in":12345678,"uid":1234}, expires_in is standard unix-epoch-time
+        '''
+        redirect = redirect_uri if redirect_uri else self._redirect_uri
+        if not redirect:
+            raise APIError('21305', 'Parameter absent: redirect_uri', 'OAuth2 request')
+        r = _http_post('%s%s' % (self._auth_url, 'access_token'), \
+                client_id = self._client_id, \
+                client_secret = self._client_secret, \
+                redirect_uri = redirect, \
+                code = code, grant_type = 'authorization_code')
+        current = int(time.time())
+        expires = r.expires_in + current
+        remind_in = r.get('remind_in', None)
+        if remind_in:
+            rtime = int(remind_in) + current
+            if rtime < expires:
+                expires = rtime
+        jo = JsonDict(access_token=r.access_token, expires_in=expires)
+        uid = r.get('uid', None)
+        if uid:
+            jo.uid = uid
+        return jo
+
+    def is_expires(self):
+        return not self._access_token or time.time() > self._expires
+
+    def __getattr__(self, attr):
+        if attr.startswith('_'):
+            raise AttributeError('WeiboClient object has no attribute \'%s\'' % attr)
+        return _Callable(self, attr)
+
+_METHOD_MAP = { 'GET': _HTTP_GET, 'POST': _HTTP_POST, 'UPLOAD': _HTTP_UPLOAD }
+
+class _Executable(object):
+
+    def __init__(self, client, method, path):
+        self._client = client
+        self._method = method
+        self._path = path
+
+    def __call__(self, **kw):
+        method = _METHOD_MAP[self._method]
+        if method==_HTTP_POST and 'pic' in kw:
+            method = _HTTP_UPLOAD
+        return _http_call('%s%s.json' % (self._client.api_url, self._path), method, self._client.access_token, **kw)
+
+    def __str__(self):
+        return '_Executable (%s %s)' % (self._method, self._path)
+
+    __repr__ = __str__
+
+class _Callable(object):
+
+    def __init__(self, client, name):
+        self._client = client
+        self._name = name
+
+    def __getattr__(self, attr):
+        if attr=='get':
+            return _Executable(self._client, 'GET', self._name)
+        if attr=='post':
+            return _Executable(self._client, 'POST', self._name)
+        name = '%s/%s' % (self._name, attr)
+        return _Callable(self._client, name)
+
+    def __str__(self):
+        return '_Callable (%s)' % self._name
+
+    __repr__ = __str__
+
